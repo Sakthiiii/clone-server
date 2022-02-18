cc6cc713a97bf1ff92d6f54f8efa88e3fd90c20e
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2022-02-18 21:11:35 +0530

    first

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ app.use('/api/v1/account', accountRoutes);
 app.use('/api/v1/email', emailRoutes);
 
 if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname,'client/build')));
 
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
   });
 }
 
